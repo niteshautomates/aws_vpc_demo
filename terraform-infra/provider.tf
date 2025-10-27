5e2fc64e393d022af76f31a1561f12c07f0cc815
Nitesh Wayafalkar <niteshwayafalkar@yahoo.com>
2025-10-27 14:43:07 +0530

    feat: commented backend block

--- terraform-infra/provider.tf
+++ terraform-infra/provider.tf
@@ -10,14 +10,14 @@ terraform {
       version = "~> 3.0"
     }
   }
-  backend "s3" {
-    # replace values with your own, or pass via -backend-config at init
-    bucket       = "20251028-terraform-state-bucket"
-    key          = "envs/aws/terraform.tfstate" # path in bucket
-    region       = "ap-south-1"
-    use_lockfile = true
-    encrypt      = true
-    # profile, role_arn, endpoint, acl are NOT valid here; use AWS env vars or -backend-config
-  }
+  # backend "s3" {
+  #   # replace values with your own, or pass via -backend-config at init
+  #   bucket       = "20251028-terraform-state-bucket"
+  #   key          = "envs/aws/terraform.tfstate" # path in bucket
+  #   region       = "ap-south-1"
+  #   use_lockfile = true
+  #   encrypt      = true
+  #   # profile, role_arn, endpoint, acl are NOT valid here; use AWS env vars or -backend-config
+  # }
 }
 
