8ef67d199184d6955e6f291c10590da9a58fb24f
Nitesh Wayafalkar <niteshwayafalkar@yahoo.com>
2025-10-27 14:15:51 +0530

    feat: formatted provider.tf

--- terraform-infra/provider.tf
+++ terraform-infra/provider.tf
@@ -12,11 +12,11 @@ terraform {
   }
   backend "s3" {
     # replace values with your own, or pass via -backend-config at init
-    bucket         = "20251028-terraform-state-bucket"
-    key            = "envs/aws/terraform.tfstate"   # path in bucket
-    region         = "ap-south-1"
+    bucket       = "20251028-terraform-state-bucket"
+    key          = "envs/aws/terraform.tfstate" # path in bucket
+    region       = "ap-south-1"
     use_lockfile = true
-    encrypt        = true
+    encrypt      = true
     # profile, role_arn, endpoint, acl are NOT valid here; use AWS env vars or -backend-config
   }
 }
