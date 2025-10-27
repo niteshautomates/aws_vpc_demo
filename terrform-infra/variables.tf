f1378fad4555b5c2b412cdac042f8cd4dd693ce2
Nitesh Wayafalkar <niteshwayafalkar@yahoo.com>
2025-10-27 13:47:32 +0530

    feat: added remote backend

--- terrform-infra/variables.tf
+++ terrform-infra/variables.tf
@@ -7,3 +7,14 @@ variable "lambda_functions" {
     environment = map(string)
   }))
 }
+
+
+variable "region" {
+  type    = string
+  default = "ap-south-1"
+}
+
+variable "dynamodb_table_name" {
+  type    = string
+  default = "terraform-state-lock"
+}
