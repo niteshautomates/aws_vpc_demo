fab9df91b1dd6d52e63f03987ab86f31d0d98657
Nitesh Wayafalkar <niteshwayafalkar@yahoo.com>
2025-10-27 15:00:48 +0530

    feat: s3 bucket prevent destroy

--- terraform-infra/main.tf
+++ terraform-infra/main.tf
@@ -4,6 +4,10 @@ resource "aws_s3_bucket" "tfstate" {
   bucket        = "20251028-terraform-state-bucket"
   force_destroy = false
 
+lifecycle {
+    prevent_destroy = true
+  }
+  
   tags = {
     Name = "terraform-state-bucket"
   }
